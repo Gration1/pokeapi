0a8f370d6b9b47a3c18ef8d1c7fcbe0137916580
Carl Hens <carl@salamander.be>
2022-11-20 16:52:01 +0100

    📋 Pokemon table

---
+++ database/migrations/2022_11_20_141020_create_pokemon_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('types');
+            $table->integer('height');
+            $table->integer('weight');
+            $table->json('moves');
+            $table->integer('order')->unique();
+            $table->string('species');
+            $table->json('stats');
+            $table->json('abilities');
+            $table->string('form');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
